26031111452b95d1031b538a190c3c5e806cbbc9
mazrean <shunsuke.w04@gmail.com>
2024-01-11 07:41:20 +0900

    ベンチマーク作成

--- formstream_test.go
+++ formstream_test.go
@@ -1,11 +1,15 @@
 package formstream_test
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
+	"net/textproto"
 	"os"
 	"strings"
+	"testing"
 
 	"github.com/mazrean/formstream"
 )
@@ -46,7 +50,8 @@ large file contents
 
 	fmt.Printf("\n\n")
 	fmt.Println("---field---")
-	fmt.Println(parser.Value("field"))
+	content, _, _ := parser.Value("field")
+	fmt.Println(content)
 
 	// Output:
 	// ---stream---
@@ -57,4 +62,148 @@ large file contents
 	//
 	// ---field---
 	// value
+}
+
+const boundary = "boundary"
+
+func sampleForm(fileSize formstream.DataSize, boundary string, reverse bool) (io.Reader, error) {
+	b := bytes.NewBuffer(nil)
+
+	mw := multipart.NewWriter(b)
+	defer mw.Close()
+
+	mw.SetBoundary(boundary)
+
+	if !reverse {
+		mw.WriteField("field", "value")
+	}
+
+	mh := make(textproto.MIMEHeader)
+	mh.Set("Content-Disposition", `form-data; name="stream"; filename="file.txt"`)
+	mh.Set("Content-Type", "text/plain")
+	w, err := mw.CreatePart(mh)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create part: %w", err)
+	}
+	_, err = io.CopyN(w, strings.NewReader(strings.Repeat("a", int(fileSize))), int64(fileSize))
+	if err != nil {
+		return nil, fmt.Errorf("failed to copy: %w", err)
+	}
+
+	if reverse {
+		mw.WriteField("field", "value")
+	}
+
+	return b, nil
+}
+
+func BenchmarkFormstream(b *testing.B) {
+	b.Run("1MB", func(b *testing.B) {
+		benchmarkFormstream(b, 1*formstream.MB, false)
+	})
+	b.Run("10MB", func(b *testing.B) {
+		benchmarkFormstream(b, 10*formstream.MB, false)
+	})
+	b.Run("100MB", func(b *testing.B) {
+		benchmarkFormstream(b, 100*formstream.MB, false)
+	})
+	b.Run("1GB", func(b *testing.B) {
+		benchmarkFormstream(b, 1*formstream.GB, false)
+	})
+
+	b.Run("1MB Reverse", func(b *testing.B) {
+		benchmarkFormstream(b, 1*formstream.MB, true)
+	})
+	b.Run("10MB Reverse", func(b *testing.B) {
+		benchmarkFormstream(b, 10*formstream.MB, true)
+	})
+	b.Run("100MB Reverse", func(b *testing.B) {
+		benchmarkFormstream(b, 100*formstream.MB, true)
+	})
+	b.Run("1GB Reverse", func(b *testing.B) {
+		benchmarkFormstream(b, 1*formstream.GB, true)
+	})
+}
+
+func benchmarkFormstream(b *testing.B, fileSize formstream.DataSize, reverse bool) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		r, err := sampleForm(fileSize, boundary, reverse)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		parser := formstream.NewParser(boundary)
+
+		parser.Register("stream", func(r io.Reader, header formstream.Header) error {
+			// get field value
+			_, _, _ = parser.Value("field")
+
+			_, err := io.Copy(io.Discard, r)
+			if err != nil {
+				return fmt.Errorf("failed to copy: %w", err)
+			}
+
+			return nil
+		}, formstream.WithRequiredPart("field"))
+
+		err = parser.Parse(r)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+	}
+}
+
+func BenchmarkStdMultipart_ReadForm(b *testing.B) {
+	// default value in http package
+	const maxMemory = 32 * formstream.MB
+
+	b.Run("1MB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 1*formstream.MB, maxMemory)
+	})
+	b.Run("10MB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 10*formstream.MB, maxMemory)
+	})
+	b.Run("100MB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 100*formstream.MB, maxMemory)
+	})
+	b.Run("1GB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 1*formstream.GB, maxMemory)
+	})
+}
+
+func benchmarkStdMultipart_ReadForm(b *testing.B, fileSize formstream.DataSize, maxMemory formstream.DataSize) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		r, err := sampleForm(fileSize, boundary, false)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		func() {
+			mr := multipart.NewReader(r, boundary)
+			form, err := mr.ReadForm(int64(maxMemory))
+			if err != nil {
+				b.Fatal(err)
+			}
+			defer form.RemoveAll()
+
+			f, err := form.File["stream"][0].Open()
+			if err != nil {
+				b.Fatal(err)
+			}
+			defer f.Close()
+
+			_, err = io.Copy(io.Discard, f)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			// get field value
+			_ = form.Value["field"][0]
+		}()
+	}
 }
--- parse.go
+++ parse.go
@@ -12,7 +12,7 @@ import (
 )
 
 func (p *Parser) Parse(r io.Reader) (err error) {
-	wh := newHookSatisfactionChecker(p.hookMap)
+	wh := newHookSatisfactionChecker(p.hookMap, p.maxMemFileSize)
 	defer func() {
 		deferErr := wh.Close()
 		// capture the error of Close()
@@ -36,7 +36,7 @@ func (p *Parser) parse(r io.Reader, wh iHookSatisfactionChecker) error {
 		var part *multipart.Part
 		part, err := mr.NextPart()
 		if err != nil {
-			if err == io.EOF {
+			if errors.Is(err, io.EOF) {
 				break
 			}
 			return fmt.Errorf("failed to read next part: %w", err)
@@ -77,9 +77,13 @@ type hookSatisfactionChecker struct {
 	satisfiedHooks   map[string]StreamHookFunc
 	unsatisfiedHooks map[string][]*waitHook
 	hookMap          map[string]*waitHook
+
+	maxMemFileSize DataSize
+	offset         int64
+	file           *os.File
 }
 
-func newHookSatisfactionChecker(streamHooks map[string]streamHook) *hookSatisfactionChecker {
+func newHookSatisfactionChecker(streamHooks map[string]streamHook, maxMemFileSize DataSize) *hookSatisfactionChecker {
 	satisfiedHooks := make(map[string]StreamHookFunc, len(streamHooks))
 	unsatisfiedHooks := make(map[string][]*waitHook)
 	hookMap := make(map[string]*waitHook, len(streamHooks))
@@ -102,7 +106,9 @@ func newHookSatisfactionChecker(streamHooks map[string]streamHook) *hookSatisfac
 
 	return &hookSatisfactionChecker{
 		satisfiedHooks:   satisfiedHooks,
-		unsatisfiedHooks: make(map[string][]*waitHook),
+		unsatisfiedHooks: unsatisfiedHooks,
+		hookMap:          hookMap,
+		maxMemFileSize:   maxMemFileSize,
 	}
 }
 
@@ -117,16 +123,49 @@ func (w *hookSatisfactionChecker) runOrSetHook(name string, part *multipart.Part
 		return nil
 	}
 
-	f, err := os.CreateTemp("", "formstream-")
-	if err != nil {
-		return fmt.Errorf("failed to create temp file: %w", err)
+	buf := bytes.NewBuffer(nil)
+	n, err := io.CopyN(buf, part, int64(w.maxMemFileSize)+1)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("failed to copy: %w", err)
+	}
+
+	var cntnt content
+	if n > int64(w.maxMemFileSize) {
+		if w.file == nil {
+			f, err := os.CreateTemp("", "formstream-")
+			if err != nil {
+				return fmt.Errorf("failed to create temp file: %w", err)
+			}
+			w.file = f
+		}
+
+		_, err := io.Copy(w.file, buf)
+		if err != nil {
+			return fmt.Errorf("failed to write: %w", err)
+		}
+
+		remainSize, err := io.Copy(w.file, part)
+		if err != nil {
+			return fmt.Errorf("failed to copy: %w", err)
+		}
+
+		cntnt = fileContent{
+			file:   w.file,
+			offset: w.offset,
+			size:   int64(buf.Len()) + remainSize,
+		}
+	} else {
+		cntnt = bytesContent{
+			content: buf.Bytes(),
+		}
+		w.maxMemFileSize -= DataSize(buf.Len())
 	}
 
 	if hook, ok := w.hookMap[name]; ok {
-		hook.callParams = &callParams{
-			reader: f,
-			header: header,
-		}
+		hook.callParams = append(hook.callParams, callParams{
+			content: cntnt,
+			header:  header,
+		})
 	} else {
 		// actually not reached
 		return fmt.Errorf("no such hook %s", name)
@@ -146,29 +185,25 @@ func (w *hookSatisfactionChecker) runSatisfiedHook(name string) error {
 			continue
 		}
 
-		if hook.callParams == nil {
-			w.satisfiedHooks[hook.name] = hook.fn
-			continue
-		}
+		w.satisfiedHooks[hook.name] = hook.fn
 
-		err := func() (err error) {
-			defer func() {
-				err := hook.callParams.reader.Close()
+		for _, param := range hook.callParams {
+			err := func() (err error) {
+				err = hook.fn(param.content.Reader(), param.header)
 				if err != nil {
-					errs = append(errs, err)
+					return fmt.Errorf("failed to execute hook of %s: %v", hook.name, err)
 				}
+
+				w.maxMemFileSize += param.content.FreeMemSize()
+
+				return nil
 			}()
-
-			err = hook.fn(hook.callParams.reader, hook.callParams.header)
 			if err != nil {
-				return fmt.Errorf("failed to execute hook of %s: %v", hook.name, err)
+				errs = append(errs, err)
 			}
-
-			return nil
-		}()
-		if err != nil {
-			errs = append(errs, err)
 		}
+
+		hook.callParams = nil
 	}
 	if len(errs) != 0 {
 		return errors.Join(errs...)
@@ -178,17 +213,11 @@ func (w *hookSatisfactionChecker) runSatisfiedHook(name string) error {
 }
 
 func (w *hookSatisfactionChecker) Close() error {
-	var errs []error
-	for _, hook := range w.hookMap {
-		if hook.unsatisfiedCount > 0 && hook.callParams != nil {
-			err := hook.callParams.reader.Close()
-			if err != nil {
-				errs = append(errs, err)
-			}
+	if w.file != nil {
+		err := w.file.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close file: %w", err)
 		}
-	}
-	if len(errs) != 0 {
-		return errors.Join(errs...)
 	}
 
 	return nil
@@ -197,11 +226,42 @@ func (w *hookSatisfactionChecker) Close() error {
 type waitHook struct {
 	name             string
 	fn               StreamHookFunc
-	callParams       *callParams
+	callParams       []callParams
 	unsatisfiedCount int
 }
 
 type callParams struct {
-	reader io.ReadCloser
-	header Header
+	content content
+	header  Header
+}
+
+type content interface {
+	Reader() io.Reader
+	FreeMemSize() DataSize
+}
+
+type bytesContent struct {
+	content []byte
+}
+
+func (c bytesContent) Reader() io.Reader {
+	return bytes.NewReader(c.content)
+}
+
+func (c bytesContent) FreeMemSize() DataSize {
+	return DataSize(len(c.content))
+}
+
+type fileContent struct {
+	file   io.ReaderAt
+	offset int64
+	size   int64
+}
+
+func (c fileContent) Reader() io.Reader {
+	return io.NewSectionReader(c.file, c.offset, c.size)
+}
+
+func (c fileContent) FreeMemSize() DataSize {
+	return 0
 }
