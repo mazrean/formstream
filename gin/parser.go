6e4e7dabd7591d73809c4523f2cf9f6b02d22648
mazrean <shunsuke.w04@gmail.com>
2024-01-13 15:31:16 +0900

    gin用Parser追加

---
+++ gin/parser.go
@@ -0,0 +1,37 @@
+package gin
+
+import (
+	"io"
+	"mime"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mazrean/formstream"
+)
+
+type Parser struct {
+	*formstream.Parser
+	reader io.Reader
+}
+
+func NewParser(c *gin.Context, options ...formstream.ParserOption) (*Parser, error) {
+	contentType := c.ContentType()
+	d, params, err := mime.ParseMediaType(contentType)
+	if err != nil || d != "multipart/form-data" {
+		return nil, http.ErrNotMultipart
+	}
+
+	boundary, ok := params["boundary"]
+	if !ok {
+		return nil, http.ErrMissingBoundary
+	}
+
+	return &Parser{
+		Parser: formstream.NewParser(boundary, options...),
+		reader: c.Request.Body,
+	}, nil
+}
+
+func (p *Parser) Parse() error {
+	return p.Parser.Parse(p.reader)
+}
