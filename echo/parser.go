c74ce220684d56a20f669778678143c54124762f
mazrean <shunsuke.w04@gmail.com>
2024-01-13 15:20:36 +0900

    echo用Parser追加

---
+++ echo/parser.go
@@ -0,0 +1,37 @@
+package echo
+
+import (
+	"io"
+	"mime"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/mazrean/formstream"
+)
+
+type Parser struct {
+	*formstream.Parser
+	reader io.Reader
+}
+
+func NewParser(c echo.Context, options ...formstream.ParserOption) (*Parser, error) {
+	contentType := c.Request().Header.Get("Content-Type")
+	d, params, err := mime.ParseMediaType(contentType)
+	if err != nil || d != "multipart/form-data" {
+		return nil, http.ErrNotMultipart
+	}
+
+	boundary, ok := params["boundary"]
+	if !ok {
+		return nil, http.ErrMissingBoundary
+	}
+
+	return &Parser{
+		Parser: formstream.NewParser(boundary, options...),
+		reader: c.Request().Body,
+	}, nil
+}
+
+func (p *Parser) Parse() error {
+	return p.Parser.Parse(p.reader)
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,19 @@ go 1.21.5
 require go.uber.org/mock v0.4.0
 
 require (
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
+
+require (
+	github.com/labstack/echo/v4 v4.11.4
 	golang.org/x/mod v0.11.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/tools v0.2.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 )
