09080edd0aecef6ef5d6c371e33fa73b16361a09
mazrean <shunsuke.w04@gmail.com>
2024-03-06 00:05:52 +0900

    http benchmark

--- http/parser_test.go
+++ http/parser_test.go
@@ -1,17 +1,22 @@
 package httpform_test
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/textproto"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/mazrean/formstream"
 	httpform "github.com/mazrean/formstream/http"
+	"github.com/mazrean/formstream/internal/myio"
 )
 
 func TestExample(t *testing.T) {
@@ -100,4 +105,254 @@ func saveUser(_ context.Context, name string, password string, iconReader io.Rea
 	user.icon = sb.String()
 
 	return nil
+}
+
+const boundary = "boundary"
+
+func sampleForm(fileSize formstream.DataSize, boundary string, reverse bool) (io.ReadSeekCloser, error) {
+	if fileSize > 1*formstream.GB {
+		f, err := os.CreateTemp("", "formstream-test-form-")
+		if err != nil {
+			return nil, fmt.Errorf("failed to create temp file: %w", err)
+		}
+
+		err = createSampleForm(f, fileSize, boundary, reverse)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create sample form: %w", err)
+		}
+
+		return f, nil
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	err := createSampleForm(buf, fileSize, boundary, reverse)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create sample form: %w", err)
+	}
+
+	return myio.NopSeekCloser(bytes.NewReader(buf.Bytes())), nil
+}
+
+func createSampleForm(w io.Writer, fileSize formstream.DataSize, boundary string, reverse bool) error {
+	mw := multipart.NewWriter(w)
+	defer mw.Close()
+
+	err := mw.SetBoundary(boundary)
+	if err != nil {
+		return fmt.Errorf("failed to set boundary: %w", err)
+	}
+
+	if !reverse {
+		err := mw.WriteField("field", "value")
+		if err != nil {
+			return fmt.Errorf("failed to write field: %w", err)
+		}
+	}
+
+	mh := make(textproto.MIMEHeader)
+	mh.Set("Content-Disposition", `form-data; name="stream"; filename="file.txt"`)
+	mh.Set("Content-Type", "text/plain")
+	pw, err := mw.CreatePart(mh)
+	if err != nil {
+		return fmt.Errorf("failed to create part: %w", err)
+	}
+	for i := 0; i < int(fileSize/formstream.MB); i++ {
+		_, err := pw.Write([]byte(strings.Repeat("a", int(formstream.MB))))
+		if err != nil {
+			return fmt.Errorf("failed to write: %w", err)
+		}
+	}
+
+	if reverse {
+		err := mw.WriteField("field", "value")
+		if err != nil {
+			return fmt.Errorf("failed to write field: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func BenchmarkFormStreamFastPath(b *testing.B) {
+	b.Run("1MB", func(b *testing.B) {
+		benchmarkFormStream(b, 1*formstream.MB, false)
+	})
+	b.Run("10MB", func(b *testing.B) {
+		benchmarkFormStream(b, 10*formstream.MB, false)
+	})
+	b.Run("100MB", func(b *testing.B) {
+		benchmarkFormStream(b, 100*formstream.MB, false)
+	})
+	b.Run("1GB", func(b *testing.B) {
+		benchmarkFormStream(b, 1*formstream.GB, false)
+	})
+	b.Run("5GB", func(b *testing.B) {
+		benchmarkFormStream(b, 5*formstream.GB, false)
+	})
+	b.Run("10GB", func(b *testing.B) {
+		benchmarkFormStream(b, 10*formstream.GB, false)
+	})
+}
+
+func BenchmarkFormStreamSlowPath(b *testing.B) {
+	b.Run("1MB", func(b *testing.B) {
+		benchmarkFormStream(b, 1*formstream.MB, true)
+	})
+	b.Run("10MB", func(b *testing.B) {
+		benchmarkFormStream(b, 10*formstream.MB, true)
+	})
+	b.Run("100MB", func(b *testing.B) {
+		benchmarkFormStream(b, 100*formstream.MB, true)
+	})
+	b.Run("1GB", func(b *testing.B) {
+		benchmarkFormStream(b, 1*formstream.GB, true)
+	})
+	b.Run("5GB", func(b *testing.B) {
+		benchmarkFormStream(b, 5*formstream.GB, true)
+	})
+	b.Run("10GB", func(b *testing.B) {
+		benchmarkFormStream(b, 10*formstream.GB, true)
+	})
+}
+
+func benchmarkFormStream(b *testing.B, fileSize formstream.DataSize, reverse bool) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parser, err := httpform.NewParser(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = parser.Register("stream", func(r io.Reader, header formstream.Header) error {
+			// get field value
+			_, _, _ = parser.Value("field")
+
+			_, err := io.Copy(io.Discard, r)
+			if err != nil {
+				return fmt.Errorf("failed to copy: %w", err)
+			}
+
+			return nil
+		}, formstream.WithRequiredPart("field"))
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		err = parser.Parse()
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+	}))
+	defer srv.Close()
+
+	r, err := sampleForm(fileSize, boundary, reverse)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer r.Close()
+
+	req, err := http.NewRequest(http.MethodPost, srv.URL, r)
+	if err != nil {
+		b.Fatal(err)
+	}
+	req.Header.Set("Content-Type", fmt.Sprintf("multipart/form-data; boundary=%s", boundary))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		_, err := r.Seek(0, io.SeekStart)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		_, err = http.DefaultClient.Do(req)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkStdMultipart_ReadForm(b *testing.B) {
+	// default value in http package
+	const maxMemory = 32 * formstream.MB
+
+	b.Run("1MB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 1*formstream.MB, maxMemory)
+	})
+	b.Run("10MB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 10*formstream.MB, maxMemory)
+	})
+	b.Run("100MB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 100*formstream.MB, maxMemory)
+	})
+	b.Run("1GB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 1*formstream.GB, maxMemory)
+	})
+	b.Run("5GB", func(b *testing.B) {
+		benchmarkStdMultipart_ReadForm(b, 5*formstream.GB, maxMemory)
+	})
+}
+
+func benchmarkStdMultipart_ReadForm(b *testing.B, fileSize formstream.DataSize, maxMemory formstream.DataSize) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mr := multipart.NewReader(r.Body, boundary)
+		form, err := mr.ReadForm(int64(maxMemory))
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer func() {
+			err := form.RemoveAll()
+			if err != nil {
+				b.Fatal(err)
+			}
+		}()
+
+		f, err := form.File["stream"][0].Open()
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer f.Close()
+
+		_, err = io.Copy(io.Discard, f)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		// get field value
+		_ = form.Value["field"][0]
+
+		w.WriteHeader(http.StatusCreated)
+	}))
+	defer srv.Close()
+
+	r, err := sampleForm(fileSize, boundary, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer r.Close()
+
+	req, err := http.NewRequest(http.MethodPost, srv.URL, r)
+	if err != nil {
+		b.Fatal(err)
+	}
+	req.Header.Set("Content-Type", fmt.Sprintf("multipart/form-data; boundary=%s", boundary))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		_, err := r.Seek(0, io.SeekStart)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		_, err = http.DefaultClient.Do(req)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
 }
